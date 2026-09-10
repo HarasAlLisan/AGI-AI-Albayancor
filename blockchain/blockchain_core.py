e59a509d09c4f4c0ffc4d3a0daa1ef8cfc0c69c0
Your Name <m1oam1o980@gmail.com>
2026-09-11 00:55:00 +0300

    FILL A75 QIYAMAH FULL 40 - A1=19 - 47f909c->A75 GREEN - 5551+40=5591 LOCK - 7 continents - 616

--- blockchain/blockchain_core.py
+++ blockchain/blockchain_core.py
@@ -86,6 +86,7 @@ class AnkhSovereignBlockchain:
  "AI-albayancor/HarasAlLisan/al-bayan/072.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/073.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/074.yaml",
+ "AI-albayancor/HarasAlLisan/al-bayan/075.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/050.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/051.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/052.yaml",
@@ -110,7 +111,8 @@ class AnkhSovereignBlockchain:
  "AI-albayancor/HarasAlLisan/al-bayan/071.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/072.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/073.yaml",
- "AI-albayancor/HarasAlLisan/al-bayan/074.yaml"
+ "AI-albayancor/HarasAlLisan/al-bayan/074.yaml",
+ "AI-albayancor/HarasAlLisan/al-bayan/075.yaml"
         ]
 
     def adam_hash(self, data: str) -> str:
