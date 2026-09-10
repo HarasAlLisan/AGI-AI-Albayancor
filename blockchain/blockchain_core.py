7ed9f0ed80e9191b5a7a462263df0a5447034e8a
Your Name <m1oam1o980@gmail.com>
2026-09-11 00:08:42 +0300

    FILL A25 FURQAN FULL 77 - A1=19 - 68acb07->A25 GREEN - 2855+77=2932 LOCK - 7 continents - 616

--- blockchain/blockchain_core.py
+++ blockchain/blockchain_core.py
@@ -35,7 +35,8 @@ class AnkhSovereignBlockchain:
  "AI-albayancor/HarasAlLisan/al-bayan/021.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/022.yaml",
  "AI-albayancor/HarasAlLisan/al-bayan/023.yaml",
- "AI-albayancor/HarasAlLisan/al-bayan/024.yaml"
+ "AI-albayancor/HarasAlLisan/al-bayan/024.yaml",
+ "AI-albayancor/HarasAlLisan/al-bayan/025.yaml"
         ]
 
     def adam_hash(self, data: str) -> str:
